077bce301aac2210afe0a6f703c67f6a598fbc99
tesnimhed <tesnissam@icloud.com>
2024-04-07 12:35:09 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './composants/Header';
 
 function App() {
   return (
     <div className="App">
     <p>Hello World</p>
+    <Header/>
     </div>
   );
 }
---
+++ src/composants/Header.jsx
@@ -0,0 +1,7 @@
+import react from "react"
+const Header=()=>{
+    return(
+        <p>je suis le header</p>
+    )
+}
+export default Header
